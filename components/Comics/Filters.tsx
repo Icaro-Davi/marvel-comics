7ace3ec21d59a9bd1a7d8636e3b45a5a0ae9bf50
raidou12 <icarodaviduarte@gmail.com>
2022-01-05 18:02:24 -0300

    added search input

--- components/Comics/Filters.tsx
+++ components/Comics/Filters.tsx
@@ -5,6 +5,8 @@ import { FaEraser, FaFilter } from 'react-icons/fa';
 import { ComicsType, getComicsParams } from '../../services/marvel-requestor/marvel.types';
 import FilterContainerModal, { FilterActionsRef } from './FilterContainerModal';
 
+const { Search } = Input;
+
 export type filtersType = {
     get: getComicsParams;
     set: Dispatch<SetStateAction<getComicsParams>>
@@ -12,6 +14,7 @@ export type filtersType = {
 
 interface FilterComicsProps {
     selectedComics: ComicsType;
+    isLoading: boolean;
     actions: {
         removeAllChecked: () => void;
         applyFilters: (params?: getComicsParams) => Promise<void>;
@@ -20,29 +23,26 @@ interface FilterComicsProps {
     filters: filtersType;
 }
 
+
 const FilterComics: React.FC<FilterComicsProps> = props => {
     const FilterContainerModalRef = useRef<FilterActionsRef>(null);
-    const [debounceId, setDebounceId] = useState<NodeJS.Timeout>();
-
-    const debounce = (func: Function) => {
-        !!debounceId && clearTimeout(debounceId);
-        setDebounceId(setTimeout(() => { func() }, 500));
-    }
-
-    const onTitleChange = (text: string) => debounce(() => {
-        props.filters.set(oldState => ({ ...oldState, titleStartsWith: text }));
-    });
-
+    const onSend = (text: string) => props.filters.set(oldState => ({ ...oldState, titleStartsWith: text }));
     return (
         <div style={{ flex: 0, display: 'flex', padding: 6 }}>
-            <FilterContainerModal ref={FilterContainerModalRef} filters={props.filters} />
+            <FilterContainerModal ref={FilterContainerModalRef} filters={props.filters} isLoading={props.isLoading} />
             <Badge size='small' count={props.selectedComics.length} offset={[-8, 3]}>
                 <Button
                     onClick={props.actions.removeAllChecked}
                     title='Limpar quadrinhos selecionados'
                 ><FaEraser /></Button>
             </Badge>
-            <Input onChange={(e) => onTitleChange(e.target.value)} placeholder='Buscar por titulo' />
+            <Search
+                allowClear
+                disabled={props.isLoading}
+                loading={props.isLoading}
+                onSearch={onSend}
+                placeholder='Buscar por titulo'
+            />
             <Button onClick={() => FilterContainerModalRef.current?.action.openModal()} title='Filtrar quadrinhos' ><FaFilter /></Button>
         </div>
     )
