c8f287f0a98a393ecc745d1d9eb8a01d3cf4c07b
raidou12 <icarodaviduarte@gmail.com>
2022-01-05 02:04:56 -0300

    custom hook to handle comics

---
+++ hooks/useComics.tsx
@@ -0,0 +1,67 @@
+import { message } from "antd";
+import { useEffect, useState } from "react";
+import MarvelApiRequestor from "../services/marvel-requestor";
+import { ComicsType, getComicsParams } from "../services/marvel-requestor/marvel.types";
+
+const useComics = (initialParams?: getComicsParams) => {
+    const [comics, setComics] = useState<ComicsType>([]);
+    const [pagination, setPagination] = useState({ offset: 0, total: 0, limit: 0 });
+    const [filters, setFilters] = useState<getComicsParams>(initialParams ? initialParams : {});
+    const [loading, setLoading] = useState(false);
+
+    const getComics = async (params?: getComicsParams) => {
+        try {
+            setLoading(true);
+            const { results, total, limit, count } = await MarvelApiRequestor.getComics(params);
+            setPagination(oldState => ({ offset: oldState.offset + count, total, limit }));
+            !!comics.length
+                ? setComics(oldState => [...oldState, ...results.filter(result => !comics.find(comics => comics.id === result.id))])
+                : setComics(results);
+            setLoading(false);
+        } catch (error) {
+            message.error('Falha ao requisitar a listar de quadrinhos.', 6);
+            setTimeout(() => {
+                message.warning('Tentando novamente', 6)
+                getComics(filters);
+            }, 12000);
+        }
+    }
+
+    const loadMore = () => {
+        getComics(filters ? { ...filters, offset: pagination.offset + 1 } : {
+            ...initialParams,
+            limit: initialParams?.limit || 10,
+            offset: pagination.offset + 1
+        });
+    }
+
+    const resetList = () => {
+        setComics([]);
+        setPagination({ offset: 0, total: 0, limit: 0 });
+    }
+
+    useEffect(() => {
+        resetList();
+        getComics(Object.keys(filters).reduce((prev, current) => ({
+            ...prev,
+            ...!!(filters as any)[current] ? { [current]: (filters as any)[current] } : {}
+        }), {}));
+    }, [filters]);
+
+    return {
+        comics,
+        isLoading: loading,
+        pagination,
+        filters: {
+            get: filters,
+            set: setFilters
+        },
+        actions: {
+            loadMore,
+            resetList,
+            getComics
+        }
+    };
+}
+
+export default useComics;
