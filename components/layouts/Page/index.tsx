b47b32c7c34d1d96c2f3c9e85f9b926aeff1620a
raidou12 <icarodaviduarte@gmail.com>
2022-01-05 02:05:26 -0300

    layout components

---
+++ components/layouts/Page/index.tsx
@@ -0,0 +1,29 @@
+import { Layout, Row, Col } from 'antd';
+
+const { Header, Footer, Content } = Layout;
+
+const PageLayout: React.FC = props => (
+    <div className='app flex-column'>
+        <Row className='app-background' justify='center'>
+            <Col xs={24} md={18} lg={14}>
+                <div className='flex-column container background-white'>
+                    <Header className='header background-white'>
+                        <div className='logo-container'>
+                            <div className='logo-position'>
+                                <img src='/svg/marvel.svg' className='logo-image' />
+                            </div>
+                        </div>
+                    </Header>
+                    <Content className='content background-white flex-column'>
+                        {props.children}
+                    </Content>
+                    <Footer className='footer background-white'>
+                        <span>Desenvolvido por <a href='https://github.com/icaro-davi' target='_blank'>Icaro Davi</a></span>
+                    </Footer>
+                </div>
+            </Col>
+        </Row>
+    </div >
+);
+
+export default PageLayout;
