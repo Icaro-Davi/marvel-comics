8ce53b2be28e793699e8605525e6525df1870879
raidou12 <icarodaviduarte@gmail.com>
2022-01-05 02:08:04 -0300

    configure app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,26 @@
-import { AppProps } from 'next/app';
 import 'antd/dist/antd.css';
+import '../styles/global.scss';
+import '../styles/components.scss';
+
+import { AppProps, AppContext } from 'next/app';
+import { ConfigProvider } from 'antd';
+import ptBR from 'antd/lib/locale/pt_BR';
+
+import PageLayout from '../components/layouts/Page';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ConfigProvider locale={ptBR}>
+      <PageLayout>
+        <Component {...pageProps} />
+      </PageLayout>
+    </ConfigProvider>
+  )
 }
 
-export default MyApp
+MyApp.getInitialProps = async ({ Component, ctx }: AppContext) => {
+  const appProps = Component.getInitialProps ? await Component.getInitialProps(ctx) : {};
+  return appProps;
+}
+
+export default MyApp;
